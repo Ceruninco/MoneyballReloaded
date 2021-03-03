24f78032c0ce70282bdbb602c3f088723fd7d7c3
Elie3 <elie.levy.3@gmail.com>
2021-03-03 18:53:22 +0100

    removing special caracters from players name

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
-This is a temporary script file.
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import preprocessing
+from unidecode import unidecode
 
+# on recupere les stats de base
 df_2016 = pd.read_csv('NBA_totals_2015-2016.csv')
 df_2017 = pd.read_csv('NBA_totals_2016-2017.csv')
 df_2018 = pd.read_csv('NBA_totals_2017-2018.csv')
 df_2019 = pd.read_csv('NBA_totals_2018-2019.csv')
 df_2020 = pd.read_csv('NBA_totals_2019-2020.csv')
 
-print(type(df_2020[["Pos"]]))
+# on enleve les accents et caractères spéciaux du nom des joueurs pour les grouper
+df_2016["Player"] = df_2016["Player"].apply(unidecode)
+df_2017["Player"] = df_2017["Player"].apply(unidecode)
+df_2018["Player"] = df_2018["Player"].apply(unidecode)
+df_2019["Player"] = df_2019["Player"].apply(unidecode)
+df_2020["Player"] = df_2020["Player"].apply(unidecode)
 
-df_2020[df_2020["Player"] == "Kyle Alexander"]
 
-#displaying only point-gards
-pg = df_2020[df_2020["Pos"]=="PG"]
+# on recupere l'équipe finale de chaque joueur
+team_and_player = df_2020[["Player", "Tm"]]
+team_and_player["final_team"] = team_and_player.groupby('Player')['Tm'].transform('last')
+team_and_player = team_and_player[["Player", "final_team"]]
+team_and_player = team_and_player.drop_duplicates()
 
-pg.columns
 
+# on ne garde que les colonnes qui nous interesse
 basic_stats_2016 = df_2016.loc[:, ['Player', 'G', 'MP', 'FGA', '3P', '3PA', '2P', '2PA', 'FT', 'FTA', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS'] ]
 basic_stats_2017 = df_2017.loc[:, ['Player', 'G', 'MP', 'FGA', '3P', '3PA', '2P', '2PA', 'FT', 'FTA', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS'] ]
 basic_stats_2018 = df_2018.loc[:, ['Player', 'G', 'MP', 'FGA', '3P', '3PA', '2P', '2PA', 'FT', 'FTA', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS'] ]
@@ -31,7 +36,9 @@ basic_stats_2019 = df_2019.loc[:, ['Player', 'G', 'MP', 'FGA', '3P', '3PA', '2P'
 basic_stats_2020 = df_2020.loc[:, ['Player', 'G', 'MP', 'FGA', '3P', '3PA', '2P', '2PA', 'FT', 'FTA', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS'] ]
 
 
+# on concatene en hauteur tous les df
 basic_stats = basic_stats_2016.append(basic_stats_2017).append(basic_stats_2018).append(basic_stats_2019).append(basic_stats_2020)
+
 
 # on group par joueur
 summed_basic_stats = basic_stats.groupby(['Player']).sum()
@@ -54,7 +61,6 @@ avg_stats_36_minutes = avg_stats_36_minutes.apply(custom_round_up, args=[1])
 names = pd.DataFrame(avg_stats_36_minutes.index)
 
 
-
 # Scaling
 avg_stats_36_minutes = avg_stats_36_minutes - avg_stats_36_minutes.min()
 avg_stats_36_minutes = avg_stats_36_minutes / ( avg_stats_36_minutes.max() - avg_stats_36_minutes.min() )
@@ -69,7 +75,15 @@ ad_2018 = pd.read_csv('NBA_advanced_2017-2018.csv')
 ad_2019 = pd.read_csv('NBA_advanced_2018-2019.csv')
 ad_2020 = pd.read_csv('NBA_advanced_2019-2020.csv')
 
+# on enleve les accents et caractères spéciaux du nom des joueurs pour les grouper
+ad_2016["Player"] = ad_2016["Player"].apply(unidecode)
+ad_2017["Player"] = ad_2017["Player"].apply(unidecode)
+ad_2018["Player"] = ad_2018["Player"].apply(unidecode)
+ad_2019["Player"] = ad_2019["Player"].apply(unidecode)
+ad_2020["Player"] = ad_2020["Player"].apply(unidecode)
 
+
+# on ne garde que les colonnes qui nous intéresse
 ad_2016 = ad_2016.loc[:, ["Player", "G", "MP", "PER", "TS%", "3PAr", "TRB%", "USG%", "OWS", "DWS"] ]
 ad_2017 = ad_2017.loc[:, ["Player", "G", "MP", "PER", "TS%", "3PAr", "TRB%", "USG%", "OWS", "DWS"] ]
 ad_2018 = ad_2018.loc[:, ["Player", "G", "MP", "PER", "TS%", "3PAr", "TRB%", "USG%", "OWS", "DWS"] ]
@@ -97,7 +111,7 @@ def ponderateByGamesPlayed(df):
     res.columns = ["Player", "G", "MP", "PER", "TS%", "3PAr", "TRB%", "USG%", "OWS", "DWS"]
     return res
 
-
+# on applique la fonction pour pondérer par le nb de match joué
 ad_2016 = ponderateByGamesPlayed(ad_2016)
 ad_2017 = ponderateByGamesPlayed(ad_2017)
 ad_2018 = ponderateByGamesPlayed(ad_2018)
@@ -112,6 +126,7 @@ summed_ad = ad_2016.append(ad_2017).append(ad_2018).append(ad_2019).append(ad_20
 # On agrege
 agr = {'MP':['sum'],'G': ['sum'], 'PER':['sum'],'TS%':['sum'],'3PAr':['sum'],'TRB%':['sum'],'USG%':['sum'], 'OWS':['sum'], 'DWS':['sum']}
 agg_advanced =summed_ad.groupby(['Player']).agg(agr)
+
 
 # on enleve ceux qui ont joué moins de 100 matches
 agg_advanced = agg_advanced[agg_advanced["G"]['sum'] > 100 ]
@@ -130,8 +145,9 @@ final_advanced.columns = ["G", "MP", "PER", "TS%", "3PAr", "TRB%", "USG%", "OWS"
 final_advanced_scaled = final_advanced - final_advanced.min()
 final_advanced_scaled = final_advanced_scaled / ( final_advanced_scaled.max() - final_advanced_scaled.min() )
 
-
+# on fusionne les stats avancées et les stats de base
 final = pd.merge(final_advanced_scaled, avg_stats_36_minutes_scaled, on="Player")
+
 
 
 
