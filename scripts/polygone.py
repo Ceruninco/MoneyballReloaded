1a9ba0d5ac3c3ec6b9d1970788c92bb52634c6e8
Elie3 <elie.levy.3@gmail.com>
2021-03-17 10:21:03 +0100

    removing useless white space

--- scripts/polygone.py
+++ scripts/polygone.py
@@ -4,10 +4,7 @@ Created on Sat Dec 19 17:16:33 2020
 
 @author: Aksel
 """
-import shapely.geometry as sg
-import descartes
 import pandas as pd
-import sklearn.utils as utils
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,21 +12,9 @@ import matplotlib
 import matplotlib.path as path
 import matplotlib.patches as patches
 
-"""
-Ad1 =  pd.read_csv('NBA_advanced_2019-2020.csv',delimiter = ',')
-Ad1.insert(2,'Year',2020)
-Ad2=pd.read_csv('NBA_advanced_2018-2019.csv',delimiter = ',')
-Ad2.insert(2,'Year',2019)
-Ad3=pd.read_csv('NBA_advanced_2017-2018.csv',delimiter = ',')
-Ad3.insert(2,'Year',2018)
-Ad4=pd.read_csv('NBA_advanced_2016-2017.csv',delimiter = ',')
-Ad4.insert(2,'Year',2017)
-"""
 PlayerStats="MP"
 
 NormalizeData = pd.read_csv("../csv/players_stats.csv", delimiter =",");
-
-#Ad = Ad1.append(Ad2).append(Ad3).append(Ad4)
 
 
 """ a function which computes a performance polygon for a specific player using three parameters
@@ -142,7 +127,6 @@ def performance_polygon_vs_player(*PlayerName):
 
     plt.title("Performance polygon", pad = 50)
     plt.show()
-
 
 
 PlayerName='LeBron James'
