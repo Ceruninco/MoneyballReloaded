4e5eca47ef046829f474ae3091cb0f58f031c55d
Belzerion <jeille.hd@gmail.com>
2021-03-17 10:00:19 +0100

    cluster and polygon

--- scripts/StatsYears.py
+++ scripts/StatsYears.py
@@ -4,7 +4,8 @@ Created on Sat Dec 19 17:16:33 2020
 
 @author: Aksel
 """
-
+import shapely.geometry as sg
+import descartes
 import pandas as pd
 import sklearn.cluster as cluster
 import sklearn.utils as utils
@@ -24,6 +25,7 @@ import chart_studio.plotly as py
 from plotly.offline import download_plotlyjs, init_notebook_mode,  plot
 from plotly.graph_objs import *
 
+"""
 Ad1 =  pd.read_csv('NBA_advanced_2019-2020.csv',delimiter = ',')
 Ad1.insert(2,'Year',2020)
 Ad2=pd.read_csv('NBA_advanced_2018-2019.csv',delimiter = ',')
@@ -32,24 +34,26 @@ Ad3=pd.read_csv('NBA_advanced_2017-2018.csv',delimiter = ',')
 Ad3.insert(2,'Year',2018)
 Ad4=pd.read_csv('NBA_advanced_2016-2017.csv',delimiter = ',')
 Ad4.insert(2,'Year',2017)
-
+"""
 PlayerStats="MP"
 
-Ad = Ad1.append(Ad2).append(Ad3).append(Ad4)
+NormalizeData = pd.read_csv("../csv/players_stats.csv", delimiter =",");
+
+#Ad = Ad1.append(Ad2).append(Ad3).append(Ad4)
 
 
 """ a function which computes a performance polygon for a specific player using three parameters
  (Offensive Win Share, Defensive Win Share, Win Share)"""
 
 def performance_polygon(PlayerName):
-    AdDisp=Ad[Ad.Player.eq(PlayerName)]
+    Player=10*NormalizeData[NormalizeData.Player.eq(PlayerName)]
 
-    Player = AdDisp[AdDisp.Year.eq(2020)]
+    # Player = AdDisp[AdDisp.Year.eq(2020)]
 
-    properties = ['Offensive Win share', 'Defensive win share', 'Win share']
+    properties = ['Offensive Win share', 'Defensive win share', 'AST','TS%', "TRB%", "PTS", "3PA", ]
     values = np.random.uniform(5,9,len(properties))
 
-    values = [Player['OWS'], Player['DWS'], Player['WS']]
+    values = [Player['OWS'], Player['DWS'], Player['AST'], Player["TS%"], Player["TRB%"], Player["PTS"], Player["3PA"]]
     matplotlib.rc('axes', facecolor = 'white')
 
     fig = plt.figure(figsize=(10,8), facecolor='white')
@@ -73,13 +77,14 @@ def performance_polygon(PlayerName):
 
     plt.scatter(points[:,0],points[:,1], linewidth=2,
                 s=50, color='white', edgecolor='black', zorder=10)
-
+    """
     maxi = max([Player.iloc[0,19]+1, Player.iloc[0,20]+1, Player.iloc[0,21]+1])
     if maxi < 10:
         plt.ylim(0,10)
     else:
         plt.ylim(0,maxi)
-
+        """
+    plt.ylim(0,10)
     for i in range(len(properties)):
         angle_rad = i/float(len(properties))*2*np.pi
         angle_deg = i/float(len(properties))*360
@@ -90,17 +95,82 @@ def performance_polygon(PlayerName):
 
     plt.title("Statistics of "+PlayerName)
     plt.show()
+    
+def performance_polygon_vs_player(*PlayerName):
+    colors = ["blue", "red", "green", "orange", "brown"]
+    fig = plt.figure(figsize=(10,8), facecolor='white')
+    for i in range (0,len(PlayerName)):
+        Player=10*NormalizeData[NormalizeData.Player.eq(PlayerName[i])]
+        #Player2=10*NormalizeData[NormalizeData.Player.eq(PlayerName[1])]
+        
+        # Player = AdDisp[AdDisp.Year.eq(2020)]
+    
+        properties = ['Offensive Win share', 'Defensive win share', 'AST','TS%', "TRB", "PTS", "3PA", ]
+        values = np.random.uniform(5,9,len(properties))
+    
+        values1 = [Player['OWS'], Player['DWS'], Player['AST'], Player["TS%"], Player["TRB"], Player["PTS"], Player["3PA"]]
+        #values2 = [Player2['OWS'], Player2['DWS'], Player2['AST'], Player2["TS%"], Player2["TRB%"], Player2["PTS"], Player2["3PA"]]
+        matplotlib.rc('axes', facecolor = 'white')
+    
+        
+    
+        axes = plt.subplot(111, polar=True)
+    
+        t = np.arange(0,2*np.pi,2*np.pi/len(properties))
+        plt.xticks(t, [])
+    
+        points = [(x,y) for x,y in zip(t,values1)]
+        points.append(points[0])
+        points = np.array(points)
+        codes = [path.Path.MOVETO,] + \
+                [path.Path.LINETO,]*(len(values) -1) + \
+                [ path.Path.CLOSEPOLY ]
+        _path = path.Path(points, codes)
+        _patch = patches.PathPatch(_path, fill=False, color=colors[i], linewidth=0, alpha=.2)
+        axes.add_patch(_patch)
+        _patch = patches.PathPatch(_path, fill=False, edgecolor=colors[i], linewidth = 2, label=PlayerName[i])
+        axes.add_patch(_patch)
+        plt.scatter(points[:,0],points[:,1], linewidth=2,
+                s=50, color='white', edgecolor='black', zorder=10)
+    #plt.scatter(points[:,0],points[:,1], linewidth=2,s=50, color='white', edgecolor='black', zorder=10)
+    plt.legend(loc="lower right",borderaxespad=-6)
+    """
+    maxi = max([Player.iloc[0,19]+1, Player.iloc[0,20]+1, Player.iloc[0,21]+1])
+    if maxi < 10:
+        plt.ylim(0,10)
+    else:
+        plt.ylim(0,maxi)
+        """
+    plt.ylim(0,10)
+    for i in range(len(properties)):
+        angle_rad = i/float(len(properties))*2*np.pi
+        angle_deg = i/float(len(properties))*360
+        ha = "right"
+        if angle_rad < np.pi/2 or angle_rad > 3*np.pi/2: ha = "left"
+        plt.text(angle_rad, 10.75, properties[i], size=14,
+                 horizontalalignment=ha, verticalalignment="center")
+
+    plt.title("Performance polygon", pad = 50)
+    plt.show()
+
 
 
 PlayerName='LeBron James'
-performance_polygon(PlayerName)
+#PlayerName5= "Buddy Hield"
+PlayerName6="Stephen Curry"
+#performance_polygon(PlayerName)
+PlayerName2 = "Aaron Brooks"
+PlayerName3 = "Brandon Knight"
+PlayerName4 = "Jamal Crawford"
+
+performance_polygon_vs_player(PlayerName, PlayerName6, PlayerName2, PlayerName3, PlayerName4)
 
 
 def histogram_minutes_played_random_chosen_players():
     shuffledData = utils.shuffle(Ad)
     sample = shuffledData[1:20]
     #sample.plot.bar(x='Player', y='G', rot=90)
-
+"""
 
 histogram_minutes_played_random_chosen_players()
 
@@ -109,12 +179,12 @@ Ad_G_MP = Ad1[['G','MP']]
 plt.scatter(Ad_G_MP['MP'],Ad_G_MP['G'])
 plt.figure()
 
-"""
+
 Ad_MP_3P = Ad1[['MP','3P']]
 #clustering = cluster.DBSCAN(eps=5, min_samples=10).fit_predict(Ad_MP_3P)
 plt.scatter(Ad_MP_3P['MP'],Ad_MP_3P['3P'])
 plt.figure()
-"""
+
 
 df = pd.read_csv('NBA_totals_2019-2020.csv')
 df[['FG%', '3P%', '2P%', 'FT%', 'eFG%']] = \
@@ -182,3 +252,4 @@ fig = dict( data=[point, mesh],  layout=layout )
 
 plot(fig,filename='clustering 3D')
 
+"""
--- scripts/cluster.py
+++ scripts/cluster.py
@@ -20,6 +20,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
+
+
 path = "./ComputedClusters"
 if not os.path.exists(path):
     os.mkdir(path)
@@ -27,46 +30,88 @@ if not os.path.exists(path):
 df = pd.read_csv('../csv/players_stats.csv')
 
 
+
 #print(df.shape)
 df = df.dropna()
-print(df)
-data = np.zeros((df.shape[0],7))
+#print(df)
+data = np.zeros((df.shape[0],8))
 #print(df[['2P%', '3P%']].values[0])
 
 for i in range(df.shape[0]):
-    data[i] = df[['TRB', 'PTS', 'AST', 'DWS', 'TS%', "3PA", "OWS"]].values[i]
+    data[i] = df[['TRB', 'PTS', 'AST', 'DWS', 'TS%', "3PA", "OWS","USG%"]].values[i]
     
-print(data)
-X = StandardScaler().fit_transform(data)
+#print(data)
+
 
 #print(X)
 
 # Compute DBSCAN
-i = 0.1
-j = 2
-while i <= 1:
-    while j <= 10:
-        db = DBSCAN(eps=i, min_samples=j).fit(X)
-        core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-        core_samples_mask[db.core_sample_indices_] = True
-        labels = db.labels_
-        #print(labels)
-        # Number of clusters in labels, ignoring noise if present.
-        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-        n_noise_ = list(labels).count(-1)
-        noise_prop = n_noise_/len(labels)
-        if(noise_prop < 0.7):
-            res = np.zeros((0,2))
-            result = pd.DataFrame(res)
-            for k in range(df.shape[0]):
-                row = [[df['Player'].values[k], labels[k]]]
-                result = result.append(row)
-            result = result.sort_values(by=[1], ascending = False)
-            pathfile = path+"/epsilon_"+str(round(i,2))+"_MinPoints_"+str(j)+"_NoiseProp_"+str(round(noise_prop,2))+".csv"
-            result.to_csv(pathfile, index =False, sep=';')
-        j+=1
-    j=2
-    i+=0.1
+def generate_Computed_Clusters_Directory():
+    X = StandardScaler().fit_transform(data)
+    i = 0.1
+    j = 2
+    while i <= 1:
+        while j <= 10:
+            db = DBSCAN(eps=i, min_samples=j).fit(X)
+            core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+            core_samples_mask[db.core_sample_indices_] = True
+            labels = db.labels_
+            #print(labels)
+            # Number of clusters in labels, ignoring noise if present.
+            n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+            n_noise_ = list(labels).count(-1)
+            noise_prop = n_noise_/len(labels)
+            if(noise_prop < 0.7):
+                res = np.zeros((0,2))
+                result = pd.DataFrame(res)
+                for k in range(df.shape[0]):
+                    row = [[df['Player'].values[k], labels[k]]]
+                    result = result.append(row)
+                result = result.sort_values(by=[1], ascending = False)
+                pathfile = path+"/epsilon_"+str(round(i,2))+"_MinPoints_"+str(j)+"_NoiseProp_"+str(round(noise_prop,2))+".csv"
+                result.to_csv(pathfile, index =False, sep=';')
+            j+=1
+        j=2
+        i+=0.1
+
+generate_Computed_Clusters_Directory()
+
+def iterative_Clustering(epsilon,minPoints,noise_prop):
+    reference = pd.read_csv('../csv/players_stats.csv')
+    path = "epsilon_"+str(epsilon)+"_MinPoints_"+str(minPoints)+"_NoiseProp_"+str(noise_prop)+".csv"
+    df = pd.read_csv('./ComputedClusters/'+path, delimiter=';')
+    tmp = pd.DataFrame()
+    for k in range(df.shape[0]):
+        if (df['1'].values[k] == 0 or df['1'].values[k] == -1):
+            row = [[df['0'].values[k]]]
+            tmp = tmp.append(row)
+    player_stats = pd.DataFrame();
+    for i in range(tmp.shape[0]):
+        for j in range(reference.shape[0]):
+            if(tmp[0].values[i] == reference['Player'].values[j]):
+                row = [reference[['TRB', 'PTS', 'AST', 'DWS', 'TS%', "3PA", "OWS","USG%"]].values[j]]
+                player_stats = player_stats.append(row)
+    #print(player_stats)
+    X = StandardScaler().fit_transform(player_stats)
+    db = DBSCAN(eps=0.9, min_samples=3).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    #print(labels)
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+    noise_prop = n_noise_/len(labels)
+    res = np.zeros((0,2))
+    result = pd.DataFrame(res)
+    for k in range(player_stats.shape[0]):
+        row = [[reference['Player'].values[k], labels[k]]]
+        result = result.append(row)
+        result = result.sort_values(by=[1], ascending = False)
+    print(result)
+    
+iterative_Clustering(0.9,2,0.52)
+
     
 """
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
