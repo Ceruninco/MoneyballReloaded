f9ceea93a8cd7c797a940b17256b63091d887cc3
Belzerion <jeille.hd@gmail.com>
2021-03-17 10:07:13 +0100

    polygone

--- scripts/polygone.py
+++ scripts/polygone.py
@@ -23,7 +23,7 @@ import seaborn as sns
 import chart_studio.plotly as py
 from plotly.offline import download_plotlyjs, init_notebook_mode,  plot
 from plotly.graph_objs import *
-
+"""
 Ad1 =  pd.read_csv('NBA_advanced_2019-2020.csv',delimiter = ',')
 Ad1.insert(2,'Year',2020)
 Ad2=pd.read_csv('NBA_advanced_2018-2019.csv',delimiter = ',')
@@ -32,12 +32,12 @@ Ad3=pd.read_csv('NBA_advanced_2017-2018.csv',delimiter = ',')
 Ad3.insert(2,'Year',2018)
 Ad4=pd.read_csv('NBA_advanced_2016-2017.csv',delimiter = ',')
 Ad4.insert(2,'Year',2017)
-
+"""
 PlayerStats="MP"
 
-NormalizeData = pd.read_csv("csv/players_stats.csv", delimiter =",");
+NormalizeData = pd.read_csv("../csv/players_stats.csv", delimiter =",");
 
-Ad = Ad1.append(Ad2).append(Ad3).append(Ad4)
+#Ad = Ad1.append(Ad2).append(Ad3).append(Ad4)
 
 
 """ a function which computes a performance polygon for a specific player using three parameters
