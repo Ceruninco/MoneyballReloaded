7738ac206662a06cc252c8d0eb07d99de2e24ed7
Belzerion <jeille.hd@gmail.com>
2021-03-28 23:15:34 +0200

    some change silouhette

--- scripts/silhouette_grid_search.py
+++ scripts/silhouette_grid_search.py
@@ -18,33 +18,31 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 #from yellowbrick.cluster import KElbowVisualizer
 from kneebow.rotor import Rotor
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 sns.set()
 
 
 # In[2]:
 
 
-df = pd.read_csv('./csv/players_stats.csv')
+df = pd.read_csv('../csv/players_stats.csv')
 
 clustering_df = df.drop(columns=["Unnamed: 0","Player", "final_team","Pos"])
-pca = PCA(n_components=0.85, svd_solver = 'full')
-pcabis = pca.fit(clustering_df)
+results = pd.DataFrame(data = None, columns = ['epsilon' , 'pca', 'min_size', 'score'],dtype=np.float64) 
+for i in np.arange(0.05,1,0.05):
+    pca = PCA(n_components=i, svd_solver = 'full')
+    pcabis = pca.fit(clustering_df)
+    
+    reducedDataSet = pcabis.transform(clustering_df)
 
-reducedDataSet = pcabis.transform(clustering_df)
+    
+    for eps in np.arange(start = 0.05,stop=0.95,step=0.01,dtype=np.float64):
+        for size in np.arange(start = 2,stop=10,step=1,dtype=np.float64): 
+            m = DBSCAN(eps=eps, min_samples=size)
+            m.fit(reducedDataSet)
+            if(max(m.labels_)>1):
+                score = sklearn.metrics.silhouette_score(reducedDataSet,m.labels_)
+                results = results.append({'epsilon' : eps , 'min_size' : size , 'score' : score, 'pca' : i}, ignore_index=True)
 
-
-# %%
-
-
-results = pd.DataFrame(data = None, columns = ['epsilon' , 'min_size', 'score'],dtype=np.float64) 
-print('dickhead')
-for eps in np.arange(start = 0.05,stop=0.95,step=0.01,dtype=np.float64):
-    for size in np.arange(start = 2,stop=10,step=1,dtype=np.float64): 
-        m = DBSCAN(eps=eps, min_samples=size)
-        m.fit(reducedDataSet)
-        if(max(m.labels_)>1):
-            score = sklearn.metrics.silhouette_score(reducedDataSet,m.labels_)
-            results = results.append({'epsilon' : eps , 'min_size' : size , 'score' : score}, ignore_index=True)
-
-
-results.to_csv("./csv/silhouette_search.csv")
+results.to_csv("../csv/silhouette_search.csv", sep = ";")
